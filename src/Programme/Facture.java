d3fb959346618d5c0ac2825f159d7750708010da
Altho <alan.at.thomas@gmail.com>
2021-06-27 00:01:45 +0200

    version finale

--- src/Programme/Facture.java
+++ src/Programme/Facture.java
@@ -1,145 +1,211 @@
 package Programme;
 
-import java.util.Arrays;
 
 public class Facture {
-	/*
-	* Pour chaque tableau, la première dimension correspond à la facture et la deuxième à la ligne. Pour obtenir
-	* la 5eme ligne de la premiere facture par exemple nous utiliserons
-	* console.log (proTab[0][4]+" "+qteTab[0][4]);
-	* */
-	private static int[][] proTab = new int[0][];
-	private static int[][] qteTab = new int[0][];
-	/*
-	 * Question 4.1 : D�finir un facturier (voir sujet)
-	 */
+    /*
+     * Pour chaque tableau, la première dimension correspond à la facture et la deuxième à la ligne. Pour obtenir
+     * la 5eme ligne de la premiere facture par exemple nous utiliserons
+     * console.log (proTab[0][4]+" "+qteTab[0][4]);
+     * */
+    static int[][] proTab = new int[0][];
+    static int[][] qteTab = new int[0][];
+    /*
+     * Question 4.1 : D�finir un facturier (voir sujet)
+     */
 
-	public static void main(String[] args) {
-		int numFacture = nouvelleFacture();
-		ajouterProduit(numFacture, 0, 10);
-		ajouterProduit(numFacture, 3, 2);
-		afficherFacture(numFacture);
-
-		numFacture = nouvelleFacture();
-		ajouterProduit(numFacture, 1, 1);
-		ajouterProduit(numFacture, 0, 5);
-		ajouterProduit(numFacture, 2, 5);
-		afficherFacture(numFacture);
-
-		afficher();
-
-	}
-
-	public static int nouvelleFacture() {
-		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-		System.out.println("NOUVELLE FACTURE");
-		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-		System.out.println(" ");
-		System.out.println("--------------------");
-		System.out.println("Facture "+ proTab.length);
-		System.out.println("--------------------");
-		System.out.println(" ");
-		int temPro[][] = new int[proTab.length+1][];
-		int tempQte[][] = new int[qteTab.length+1][];
-		temPro[temPro.length-1] = new int[0];
-		for(int i = 0; i<proTab.length; i++){
-
-			temPro[i] = proTab[i];
-			tempQte[i] = qteTab[i];
+    public static void main(String[] args) {
 
 
-		}
-		temPro[temPro.length-1] = new int[0];
-		tempQte[tempQte.length-1] = new int[0];
-		proTab=temPro;
-		qteTab=tempQte;
+    }
 
-		//TODO delete
-		System.out.println(Arrays.deepToString(proTab));
-		System.out.println(Arrays.deepToString(qteTab));
+    public static int nouvelleFacture() {
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println("NOUVELLE FACTURE");
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println(" ");
+        System.out.println("--------------------");
+        System.out.println("Facture " + (proTab.length + 1));
+        System.out.println("--------------------");
+        System.out.println(" ");
+        int temPro[][] = new int[proTab.length + 1][];
+        int tempQte[][] = new int[qteTab.length + 1][];
+        temPro[temPro.length - 1] = new int[0];
+        for (int i = 0; i < proTab.length; i++) {
 
-		return proTab.length;
-
-	}
-
-	public static int accesFactures() {
-		for(int i = 1; i< proTab.length; i++){
-		System.out.println("Facture "+ i);
-		System.out.println("Contient "+ proTab[i-1].length+ " produits uniques");}
-		System.out.println(" ");
-
-		return proTab.length;
-	};
-
-	public static void ajouterProduit(int facture, int produit,
-			int qte) {
-		int temPro[] = new int[proTab[facture].length+1];
-		int tempQte[] = new int[qteTab[qteTab.length-1].length+1];
-
-		System.out.println(temPro.length);
-		System.out.println(proTab[facture].length);
+            temPro[i] = proTab[i];
+            tempQte[i] = qteTab[i];
 
 
-		for(int i = 0; i<proTab[facture].length; i++){
-			temPro[i] = proTab[facture][i];
-			tempQte[i] = qteTab[facture][i];
-		}
-		temPro[temPro.length-1]=produit;
-		tempQte[tempQte.length-1]=qte;
-		proTab[proTab.length-1]=temPro;
-		qteTab[qteTab.length-1]=tempQte;
+        }
+        temPro[temPro.length - 1] = new int[0];
+        tempQte[tempQte.length - 1] = new int[0];
+        proTab = temPro;
+        qteTab = tempQte;
 
 
-		Programme.Menu.menu();
-	}
+        return proTab.length;
+
+    }
+
+    public static int accesFactures() {
 
 
+        String fmt = "%-10s %-25s %-10s%n";
 
-	/**
-	 * Affiche une facture avec son num�ro, la liste des produits (nom du
-	 * produit, quantit�, prix unitaire et prix total de la ligne), et le total
-	 * de la facture.
-	 * 
-	 * @param numFacture
-	 *            num�ro de la facture
-	 */
-	public static void afficherFacture(int numFacture) {
-		double total = 0.0;
-		System.out.println("_______________");
-		System.out.println("|             |");
-		System.out.println("|  Facture " +numFacture+"   |");
-		System.out.println("|             |");
-		System.out.println("_______________");
-		System.out.println(" ");
+        int qteArticle = 0;
+        double total = 0;
+        System.out.println("-----------------------------------------------");
+        System.out.printf(Menu.Couleurs.G_BLANC+fmt+Menu.Couleurs.RESET, "N° Facture ", "Nombre d'articles", "Total");
+        System.out.println("-----------------------------------------------");
+
+        for (int i = 0; i < proTab.length; i++) {
+            for (int e = 0; e < qteTab[i].length; e++) {
+                qteArticle += qteTab[i][e];
+                double prix = Catalogue.tabPrix[proTab[i][e]];
+                double articleTotal = prix * qteTab[i][e];
+                total += articleTotal;
 
 
-		for (int i = 0; i< proTab[numFacture-1].length; i++){
-			String produit = Catalogue.tabNoms[proTab[numFacture-1][i]];
-			int qte = qteTab[numFacture-1][i];
-			double prix = Catalogue.tabPrix[proTab[numFacture-1][i]];
-			double articleTotal =prix*qte;
+            }
 
-			total += articleTotal;
+            if (i % 2 == 0) {
+                System.out.printf(fmt, (i + 1), qteArticle, total);
 
-			System.out.println(produit);
-			System.out.println("Quantité : "+ qte);
-			System.out.println("Prix unitaire : "+ prix);
-			System.out.println("Total : "+ articleTotal+" Euros");
+            } else {
+                System.out.printf(Menu.Couleurs.CYAN + fmt + Menu.Couleurs.RESET, (i + 1), qteArticle, total);
 
-			System.out.println("_______________");
-			System.out.println(" ");
-		}
-		System.out.println(" ");
-		System.out.println("TOTAL : "+total+" Euros.");
-	}
+            }
+            qteArticle = 0;
+            total = 0;
 
-	/**
-	 * affiche toutes les factures.
-	 */
-	public static void afficher() {
-		// TODO Question 4.5
+        }
+        return proTab.length;
 
-	}
+
+    }
+
+    ;
+
+    public static void ajouterProduit(int facture, int produit,
+                                      int qte) {
+        int temPro[] = new int[proTab[facture].length + 1];
+        int tempQte[] = new int[qteTab[qteTab.length - 1].length + 1];
+
+
+        for (int i = 0; i < proTab[facture].length; i++) {
+            temPro[i] = proTab[facture][i];
+            tempQte[i] = qteTab[facture][i];
+        }
+        temPro[temPro.length - 1] = produit;
+        tempQte[tempQte.length - 1] = qte;
+        proTab[facture] = temPro;
+        qteTab[facture] = tempQte;
+        System.out.println(Menu.Couleurs.G_VERT + "Produit ajouté" + Menu.Couleurs.RESET);
+        System.out.println(" ");
+
+        System.out.println("Souhaitez vous ajouter un autre produit à cette facture ?(O/N)");
+        String rep = Terminal.lireString();
+        if (rep.equals("O") || rep.equals("o")) {
+            System.out.println("Veuillez entrer l'id du produit à ajouter");
+            int prod = Terminal.lireInt();
+            if (prod > Catalogue.tabNoms.length - 1) {
+                System.out.println(" ");
+                System.out.println(Menu.Couleurs.G_ROUGE + "CE PRODUIT N'EXISTE PAS" + Menu.Couleurs.RESET);
+                Menu.retourMenu();
+            }
+            System.out.println("Veuillez entrer la quantité");
+            int quanta = Terminal.lireInt();
+            Programme.Facture.ajouterProduit(facture, prod, quanta);
+
+
+        } else {
+            Programme.Menu.retourMenu();
+        }
+        Programme.Menu.retourMenu();
+    }
+
+
+    /**
+     * Affiche une facture avec son num�ro, la liste des produits (nom du
+     * produit, quantit�, prix unitaire et prix total de la ligne), et le total
+     * de la facture.
+     *
+     * @param numFacture num�ro de la facture
+     */
+    public static void afficherFacture(int numFacture) {
+        double total = 0.0;
+
+        try {
+
+            System.out.println("_______________");
+            System.out.println("Facture " + numFacture);
+            System.out.println("_______________");
+            System.out.println(" ");
+
+            String fmt = "%-30s %-10s %-10s %-10s%n";
+            System.out.println("---------------------------------------------------------------");
+            System.out.printf(Menu.Couleurs.G_BLANC + fmt + Menu.Couleurs.RESET, "Article ", "Quantité", "Prix unit.", "Total art.");
+            System.out.println("---------------------------------------------------------------");
+
+            for (int i = 0; i < proTab[numFacture - 1].length; i++) {
+                String produit = Catalogue.tabNoms[proTab[numFacture - 1][i]];
+                int qte = qteTab[numFacture - 1][i];
+                double prix = Catalogue.tabPrix[proTab[numFacture - 1][i]];
+                double articleTotal = prix * qte;
+
+                total += articleTotal;
+
+                if (i % 2 == 0) {
+                    System.out.printf(fmt, produit, qte, prix, articleTotal);
+
+                } else {
+                    System.out.printf(Menu.Couleurs.CYAN + fmt + Menu.Couleurs.RESET, produit, qte, prix, articleTotal);
+
+                }
+
+
+            }
+            System.out.println("---------------------------------------------------------------");
+            System.out.printf(Menu.Couleurs.G_JAUNE + fmt + Menu.Couleurs.RESET, " ", " ", "TOTAL : ", total + " Euros.");
+            Programme.Menu.retourMenu();
+
+
+        } catch (Exception e) {
+            if (numFacture > proTab.length) {
+                System.out.println(" ");
+                System.out.println(Menu.Couleurs.G_ROUGE + "ERREUR : Cette facture n'existe pas " + Menu.Couleurs.RESET);
+                Programme.Menu.retourMenu();
+
+            } else {
+                System.out.println("Une erreur s'est produite ");
+                System.out.println(e);
+                Programme.Menu.retourMenu();
+
+            }
+
+        }
+    }
+
+    public static void ajouterProduitPeupler(int facture, int produit,
+                                             int qte) {
+
+        int temPro[] = new int[proTab[facture].length + 1];
+        int tempQte[] = new int[qteTab[qteTab.length - 1].length + 1];
+
+
+        for (int i = 0; i < proTab[facture].length; i++) {
+            temPro[i] = proTab[facture][i];
+            tempQte[i] = qteTab[facture][i];
+        }
+        temPro[temPro.length - 1] = produit;
+        tempQte[tempQte.length - 1] = qte;
+        proTab[facture] = temPro;
+        qteTab[facture] = tempQte;
+
+
+    }
+
+
 
 }
 
--- src/Programme/Menu.java
+++ src/Programme/Menu.java
@@ -2,55 +2,51 @@ package Programme;
 
 public class Menu {
     public static void main(String[] args) {
-        menu();
-
+        lanceMenu();
     }
 
 
-
-    public static void nom(){
+    public static void nom() {
         System.out.println("Veuillez entrer l'id de l'article à afficher");
         int id = Terminal.lireInt();
-        String result =Programme.Catalogue.getNom(id);
-        System.out.println("L'ID : " + id+" "+"Correspond à : "+result);
-        menu();
+        String result = Programme.Catalogue.getNom(id);
+        System.out.println("L'ID : " + id + " " + "Correspond à : " + result);
+        retourMenu();
 
     }
 
-    public static void menu(){
+
+    public static void menu() {
         boolean quitter = false;
         do {
+
+            String fmt = "%-40s %-1s  %-10s%n";
             System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
             System.out.println("MENU PRINCIPAL");
             System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
             System.out.println(" ");
-            System.out.println("Que souhaitez vous faire ?");
-            System.out.println("---------------------");
-            System.out.println("Catalogue");
-            System.out.println("---------------------");
+            System.out.println("---------------------------------------------------------------------------------");
+
+            System.out.printf(Menu.Couleurs.G_BLANC + fmt + Menu.Couleurs.RESET, "Catalogue ", "|", "Facturier");
+
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.printf(fmt, "1. Consulter le catalogue ", "|", "6. Créer une nouvelle facturer");
+            System.out.printf(fmt, "2. Ajouter un article au catalogue", "|", "7. Liste des facturesr");
+            System.out.printf(fmt, "3. Chercher prix par ID ", "|", "8. Ajouter produit à une facture");
+            System.out.printf(fmt, "4. Chercher prix par ID ", "|", "9. Consulter une facture");
+            System.out.printf(fmt, "5. Chercher par nom d'articleu ", "|", " ");
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println(Couleurs.BB_BLEU + "11. EXPERIMENTAL : PEUPLER FACTURES (A FAIRE SUR FACTURIER VIDE) " + Couleurs.RESET);
+            System.out.println("0. Quitter le programme ");
+            System.out.println("---------------------------------------------------------------------------------");
             System.out.println(" ");
-            System.out.println("1. Consulter le catalogue");
-            System.out.println("2. Ajouter un article au catalogue");
-            System.out.println("3. Rechercher nom par ID");
-            System.out.println("4. Rechercher prix par ID ");
-            System.out.println("5. Rechercher par nom ");
+            System.out.println(Couleurs.G_BLANC + "Veuillez entrer votre choic : " + Couleurs.RESET);
             System.out.println(" ");
 
-            System.out.println("---------------------");
-            System.out.println("Facturier");
-            System.out.println("---------------------");
-            System.out.println(" ");
-            System.out.println("6. Créer une nouvelle facture ");
-            System.out.println("7. Consulter la liste des factures ");
-            System.out.println("8. Ajouter un produit à une facture ");
-            System.out.println("8. Afficher une facturee ");
-            System.out.println(" ");
-            System.out.println("---------------------");
-            System.out.println("0. Quitter ");
 
             int choix = Terminal.lireInt();
 
-            switch (choix){
+            switch (choix) {
                 case 1:
                     Programme.Catalogue.afficher();
                     break;
@@ -78,6 +74,9 @@ public class Menu {
                 case 9:
                     montrerFacture();
                     break;
+                case 11:
+                    peupler();
+                    break;
                 case 0:
                     quitter = true;
                     break;
@@ -86,74 +85,193 @@ public class Menu {
             }
 
 
-
-
-
-
-
-        } while(quitter = false);
+        } while (quitter = false);
     }
 
-    public static void ajouterLigne(){
+    public static void ajouterLigne() {
         System.out.println("Veuillez entrer le numéro de facture");
         int facture = Terminal.lireInt();
+        if (facture > Facture.proTab.length) {
+            System.out.println(" ");
+            System.out.println(Couleurs.G_ROUGE + "CETTE FACTURE N'EXISTE PAS" + Couleurs.RESET);
+            retourMenu();
+        }
         System.out.println("Veuillez entrer l'id du produit à ajouter");
-        int produit= Terminal.lireInt();
+        int produit = Terminal.lireInt();
+        if (produit > Catalogue.tabNoms.length - 1) {
+            System.out.println(" ");
+            System.out.println(Couleurs.G_ROUGE + "CE PRODUIT N'EXISTE PAS" + Couleurs.RESET);
+            retourMenu();
+        }
         System.out.println("Veuillez entrer la quantité");
         int qte = Terminal.lireInt();
-        Programme.Facture.ajouterProduit(facture-1, produit,qte);
+        Programme.Facture.ajouterProduit(facture - 1, produit, qte);
 
     }
 
-    public static void afficherFactures(){
+    public static void afficherFactures() {
         System.out.println("---------------------");
         System.out.println("Facturier");
         System.out.println("---------------------");
         System.out.println(" ");
         Programme.Facture.accesFactures();
-        menu();
-    }
-    public static void creerFacture(){
-        int factNum = Programme.Facture.nouvelleFacture();
-        System.out.println("Facture "+ factNum+" créée");
-        menu();
+        retourMenu();
     }
 
-    public static void montrerFacture(){
+    public static void creerFacture() {
+        int factNum = Programme.Facture.nouvelleFacture();
+        System.out.println(Couleurs.G_VERT + "Facture " + factNum + " créée" + Couleurs.RESET);
+        retourMenu();
+    }
+
+    public static void montrerFacture() {
         System.out.println("Veuillez entrer le numéro de facture");
         int num = Terminal.lireInt();
         Programme.Facture.afficherFacture(num);
     }
 
-    public static void prix(){
+    public static void prix() {
         System.out.println("Veuillez entrer l'id de l'article");
         int id = Terminal.lireInt();
-        double result =Programme.Catalogue.getPrix(id);
-        System.out.println("Le prix de l'article id : " + id+"("+Programme.Catalogue.getNom(id)+") "+"est de : "+result);
-        menu();
+        double result = Programme.Catalogue.getPrix(id);
+        System.out.println("Le prix de l'article id : " + id + "(" + Programme.Catalogue.getNom(id) + ") " + "est de : " + result);
+        retourMenu();
     }
 
-    public static void ajout(){
-        System.out.println("Veuillez entrer le nom de l'article à ajouter");
+    public static void retourMenu() {
+        System.out.println(" ");
+
+        System.out.println("Souhaitez vous revenir au menu ? (O/N)");
+        String rep = Terminal.lireString();
+        if (rep.equals("O") || rep.equals("o")) {
+            menu();
+        } else if (rep.equals("n") || rep.equals("N")) {
+            return;
+        } else {
+            System.out.println(Couleurs.G_ROUGE + "Saisie non valide." + Couleurs.RESET);
+            retourMenu();
+        }
+
+    }
+
+    public static void ajout() {
+        System.out.println("Nom de l'article à ajouter.");
+
         String nom = Terminal.lireString();
-        System.out.println("Veuillez entrer le prix de l'article à ajouter");
-        Double prix = Terminal.lireDouble();
-        Programme.Catalogue.ajouter(nom, prix);
-        menu();
+        String resultUC = nom.toUpperCase();
+        int querry = Programme.Catalogue.chercher(resultUC);
+
+
+        if (querry != -1) {
+            System.out.println("Cet article existe déja");
+            System.out.println("Saisir un autre article ?(O/N)");
+            String rep = Terminal.lireString();
+            if (rep.equals("O") || rep.equals("o")) {
+                Menu.ajout();
+                return;
+
+
+            } else {
+                Programme.Menu.retourMenu();
+            }
+
+        } else {
+            System.out.println("Veuillez entrer le prix de l'article à ajouter");
+            Double prix = Terminal.lireDouble();
+            Programme.Catalogue.ajouter(nom, prix);
+            retourMenu();
+        }
     }
 
-    public static void recherche(){
+    public static void lanceMenu() {
+
+        try {
+            menu();
+        } catch (Exception e) {
+            System.out.println(Couleurs.G_ROUGE + "Entrée non valide" + Couleurs.RESET);
+            Terminal.ecrireException(e);
+            System.out.println(Couleurs.G_BLANC + "Nouvelle saisie ?(O pour valider, autre touche pour annuler)" + Couleurs.RESET);
+            String rep = Terminal.lireString();
+            if (rep.equals("O") || rep.equals("o")) {
+                lanceMenu();
+            } else {
+                return;
+            }
+
+        }
+
+    }
+
+
+    public static void recherche() {
+        String fmt = "%-10s %-20s %-10s%n";
+
         System.out.println("Veuillez entrer le nom de l'article à rechercher");
         String result = Terminal.lireString();
         String resultUC = result.toUpperCase();
         int querry = Programme.Catalogue.chercher(resultUC);
-        if(querry == -1){
-            System.out.println("Aucun resultat pour : "+ result);
+        if (querry == -1) {
+            System.out.println("Aucun resultat pour : " + result);
+        } else {
+            System.out.println(" ");
+            System.out.println(Couleurs.G_VERT + "Article trouvé ! " + result + Couleurs.RESET);
+            System.out.println(" ");
+
+            System.out.printf(Menu.Couleurs.G_BLANC + fmt + Menu.Couleurs.RESET, "ID ", "Article", "Prix");
+            System.out.println("----------------------------------------");
+
+            System.out.printf(fmt, querry, Programme.Catalogue.getNom(querry), Programme.Catalogue.getPrix(querry) + " Euros.");
+
+
         }
-        else {
-            System.out.println(result+" trouvé en position "+querry);
-            System.out.println(Programme.Catalogue.getNom(querry)+" | "+Programme.Catalogue.getPrix(querry)+" Euros.");
+        retourMenu();
+    }
+
+    public static void peupler() {
+        try {
+            int max1 = Catalogue.tabNoms.length - 1;
+            int max2 = 250;
+            int min = 0;
+            int minObj = 1;
+            int range1 = max1 - min + 1;
+            int range2 = max2 - min + 1;
+            int rangeObj = max1 - minObj + 1;
+
+
+            for (int i = 1; i < 24; i++) {
+                int nbrObj = (int) Math.floor(Math.random() * rangeObj) + minObj;
+
+                Facture.nouvelleFacture();
+                for (int e = 0; e < nbrObj; e++) {
+                    int qte = (int) Math.floor(Math.random() * range2) + min;
+                    int id = (int) Math.floor(Math.random() * range1) + min;
+                    Facture.ajouterProduitPeupler((i - 1), id, qte);
+
+                }
+            }
+            System.out.println(Couleurs.G_VERT + "Facturier peuplé." + Couleurs.RESET);
+            lanceMenu();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+
         }
-        menu();
+
+
+    }
+
+    public class Couleurs {
+
+        public static final String RESET = "\033[0m";
+
+        public static final String CYAN = "\033[0;96m";
+
+        public static final String G_ROUGE = "\033[1;31m";
+        public static final String G_VERT = "\033[1;32m";
+        public static final String G_JAUNE = "\033[1;33m";
+        public static final String G_BLANC = "\033[1;97m";
+        public static final String BB_BLEU = "\033[0;107m";
+        ;
+
+
     }
 }
